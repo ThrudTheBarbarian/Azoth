2781ea1e8b082415e89bf1539264cce3e359f9bd
ThrudTheBarbarian <github@gornall.net>
2024-12-29 20:43:54 -0800

    Just a check that the icons font works as well

--- AZDemo/ColouredView.m
+++ AZDemo/ColouredView.m
@@ -21,22 +21,23 @@
 		self.backgroundColour = colour;
 
 		// Create the image (just a reference to a texture)
-		_img = [AZImage imageWithSize:NSMakeSize(100,100)
-					   drawingHandler:
-					   ^BOOL(NSRect dstRect, AZPainter * _Nonnull P)
-			{
-			int mx = self.img.width;
-			int my = self.img.height;
+//		_img = [AZImage imageWithSize:NSMakeSize(100,100)
+//					   drawingHandler:
+//					   ^BOOL(NSRect dstRect, AZPainter * _Nonnull P)
+//			{
+//			int mx = self.img.width;
+//			int my = self.img.height;
+//
+//			// Draw a cross onto the image
+//			float dx = self.angle/5;
+//
+//			[P lineAtX:dx y:0 toX:mx-dx y:my colour:AZColour.greenColour];
+//			[P lineAtX:mx y:0 toX:0 y:my colour:AZColour.blueColour];
+//			return YES;
+//
+//			} clearBeforeDraw:YES];
 
-			// Draw a cross onto the image
-			float dx = self.angle/5;
-
-			[P lineAtX:dx y:0 toX:mx-dx y:my colour:AZColour.greenColour];
-			[P lineAtX:mx y:0 toX:0 y:my colour:AZColour.blueColour];
-			return YES;
-
-			} clearBeforeDraw:YES];
-
+			_img = [AZImage imageWithSystemSymbolName:@"light"];
 //		int mx = _img.width;
 //		int my = _img.height;
 //
@@ -63,7 +64,7 @@
 //		NSLog(@"Drawing image (%d,%d) into view (%@)", (int)_img.width,
 //			  (int)_img.height, NSStringFromRect(self.frame));
 
-	//[painter image:_img to:self.visibleRect];
+	[painter image:_img to:NSMakeRect(50,50,24,24)];
 
 	//[painter image:_img at:NSMakePoint(0,0)];
 
@@ -81,11 +82,11 @@
 //	[painter image:_img at:dst.origin angle:_angle about:about flip:AZFlipNone];
 //	NSRect dst 		= NSMakeRect(100, 100, 100, 100);
 
-	NSRect dst 		= NSMakeRect(100, 100, 100, 100);
-	NSRect src		= NSMakeRect(25, 25, 50, 50);
-	NSPoint about 	= NSMakePoint(10,0);
-	[painter image:_img from:src at:dst.origin angle:_angle about:about flip:AZFlipNone];
-	[painter rectangleWithRect:dst colour:AZColour.redColour];
+//	NSRect dst 		= NSMakeRect(100, 100, 100, 100);
+//	NSRect src		= NSMakeRect(25, 25, 50, 50);
+//	NSPoint about 	= NSMakePoint(10,0);
+//	[painter image:_img from:src at:dst.origin angle:_angle about:about flip:AZFlipNone];
+//	[painter rectangleWithRect:dst colour:AZColour.redColour];
 	}
 
 @end
